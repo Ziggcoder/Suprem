0ba39136e9b9aa8a638f89fcee4c352dd21070c2
rohitwebdeveloper <you@example.com>
2024-09-03 19:42:11 +0530

    Add count chart component

--- src/app/(dashboard)/Admin/page.tsx
+++ src/app/(dashboard)/Admin/page.tsx
@@ -1,10 +1,11 @@
 import UserCard from "@/components/UserCard";
+import CountCharts from "@/components/CountCharts";
 
 const Admin = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row ">
       {/* LEFT */}
-      <div className=" w-full lg:w-2/3 bg-red-200">
+      <div className=" w-full lg:w-2/3 bg-white p-3  rounded-md" >
         {/* USERS-CARD */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="students" />
@@ -12,9 +13,17 @@ const Admin = () => {
           <UserCard type="teachers" />
           <UserCard type="staff" />
         </div>
+        <div>
+          <div>
+           <CountCharts/>
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3 bg-blue-300">2</div>
+      <div className="w-full lg:w-1/3 bg-white rounded-md ">2</div>
     </div>
   );
 }
--- src/components/CountCharts.tsx
+++ src/components/CountCharts.tsx
@@ -1,84 +1,84 @@
-// import {
-//   RadialBarChart,
-//   RadialBar,
-//   Legend,
-//   ResponsiveContainer,
-// } from "recharts";
+'use client'
+import Image from "next/image";
+import {
+    RadialBarChart,
+    RadialBar,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
 
-// const data = [
-//   {
-//     name: "18-24",
-//     uv: 31.47,
-//     pv: 2400,
-//     fill: "#8884d8",
-//   },
-//   {
-//     name: "25-29",
-//     uv: 26.69,
-//     pv: 4567,
-//     fill: "#83a6ed",
-//   },
-//   {
-//     name: "30-34",
-//     uv: 15.69,
-//     pv: 1398,
-//     fill: "#8dd1e1",
-//   },
-//   {
-//     name: "35-39",
-//     uv: 8.22,
-//     pv: 9800,
-//     fill: "#82ca9d",
-//   },
-//   {
-//     name: "40-49",
-//     uv: 8.63,
-//     pv: 3908,
-//     fill: "#a4de6c",
-//   },
-//   {
-//     name: "50+",
-//     uv: 2.63,
-//     pv: 4800,
-//     fill: "#d0ed57",
-//   },
-//   {
-//     name: "unknow",
-//     uv: 6.67,
-//     pv: 4800,
-//     fill: "#ffc658",
-//   },
-// ];
+const data = [
+    // {
+    //     name: "18-24",
+    //     uv: 31.47,
+    //     pv: 2400,
+    //     fill: "#ffffff",
+    // },
+    {
+        name: "25-29",
+        uv: 26.69,
+        pv: 4567,
+        fill: "#FAE27C",
+    },
+    {
+        name: "30-34",
+        uv: 15.69,
+        pv: 1398,
+        fill: "#C3EBFA",
+    },
+    //   {
+    //     name: "35-39",
+    //     uv: 8.22,
+    //     pv: 9800,
+    //     fill: "#82ca9d",
+    //   },
+    //   {
+    //     name: "40-49",
+    //     uv: 8.63,
+    //     pv: 3908,
+    //     fill: "#a4de6c",
+    //   },
+    //   {
+    //     name: "50+",
+    //     uv: 2.63,
+    //     pv: 4800,
+    //     fill: "#d0ed57",
+    //   },
+    //   {
+    //     name: "unknow",
+    //     uv: 6.67,
+    //     pv: 4800,
+    //     fill: "#ffc658",
+    //   },
+];
 
-// const CountCharts = () => {
-//   return (
-//     <div className="">
-//       <ResponsiveContainer width="100%" height="100%">
-//         <RadialBarChart
-//           cx="50%"
-//           cy="50%"
-//           innerRadius="10%"
-//           outerRadius="80%"
-//           barSize={10}
-//           data={data}
-//         >
-//           <RadialBar
-//             minAngle={15}
-//             label={{ position: "insideStart", fill: "#fff" }}
-//             background
-//             clockWise
-//             dataKey="uv"
-//           />
-//           <Legend
-//             iconSize={10}
-//             layout="vertical"
-//             verticalAlign="middle"
-//             wrapperStyle={style}
-//           />
-//         </RadialBarChart>
-//       </ResponsiveContainer> 
-//     </div>
-//   );
-// };
+const CountCharts = () => {
+    return (
+        <div className="max-w-3xl w-full h-full flex-col justify-center items-center  p-3 mt-2 " >
+            <div className="relative flex justify-center items-center">
+                <Image src='/maleFemale.png' alt="" width={40} height={40} className="absolute" />
+                <RadialBarChart
+                    width={300}
+                    height={300}
+                    innerRadius="50%"
+                    outerRadius="90%"
+                    data={data}
+                    startAngle={0}
+                    endAngle={360}
+                >
+                    <RadialBar label={{ fill: 'none', position: "center" }} background dataKey='uv' />
+                    {/* <Legend iconSize={7} width={200} height={200} layout="centric" verticalAlign='bottom' align="center" /> */}
+                    {/* <Tooltip /> */}
+                </RadialBarChart>
+            </div>
+            <div className="flex gap-4 justify-center ">
+            {data.map((item) => (
+                <div style={{backgroundColor:item.fill, padding:'5px', color:"black", borderRadius:'5px', fontSize:'0.8 rem'}} >{item.pv}</div>
+            )
+            )}
+            </div>
+        </div>
+    );
+};
 
-// export default CountCharts;
+export default CountCharts;
